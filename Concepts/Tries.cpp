a2365d72454a8a40e3fba150d561f9f0c9ae4189
Bhawna11agg <47032273+Bhawna11agg@users.noreply.github.com>
2020-06-14 11:23:15 +0700

    Update Tries.cpp

--- Concepts/Tries.cpp
+++ Concepts/Tries.cpp
@@ -1,5 +1,21 @@
 // root of Trie is always '\0'
 
+//TrieNode class
+class TrieNode {
+	public :
+	char data;
+	vector<TrieNode *>children(26);
+	bool isTerminal;
+
+	TrieNode(char data) {
+		this -> data = data;
+		children = new TrieNode*[26];
+		for(int i = 0; i < 26; i++) {
+			children.push_back( NULL);
+		}
+		isTerminal = false;
+	}
+};
 // insert word in trie
 void insert(string word,TrieNode* root){
     if(word[0]=='\0'){
@@ -51,6 +67,19 @@ void remove(string word,TrieNode* root){
         return;
     }
 }
+
+//Pattern Matching: It founds the pattern like if word is are then if we going to find ar then it must return true
+bool patternMatching(vector<string> vect, string pattern) {
+		for(int i=0;i<vect.size();i++){
+            for(int j=0;j<vect[i].length();j++){
+                insert(vect[i].substr(j),root);
+            }
+        }
+          if( show(pattern,root))
+              return true;
+        else
+            return false;
+	}
 
 //There are two types of Trie:
 Compressed Trie and Suffix Trie.
